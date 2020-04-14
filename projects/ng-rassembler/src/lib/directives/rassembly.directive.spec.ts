79b8fcc3b7c62b555fe3ad480b973da8b849c802
Josiah Saunders <yojo@icloud.com>
2020-04-14 00:12:30 -0600

    removed spec.ts

--- projects/ng-rassembler/src/lib/directives/rassembly.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RassemblyDirective } from './rassembly.directive';
-
-describe('RassemblyDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RassemblyDirective();
-    expect(directive).toBeTruthy();
-  });
-});
