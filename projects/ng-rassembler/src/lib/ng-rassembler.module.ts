f649218d5f8d2ea684a03a79eeed32b28e3ccbc8
Josiah Saunders <yojo@icloud.com>
2020-04-13 23:54:38 -0600

    rassembler lib

---
+++ projects/ng-rassembler/src/lib/ng-rassembler.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgRassemblerComponent } from './ng-rassembler.component';
+
+
+
+@NgModule({
+  declarations: [NgRassemblerComponent],
+  imports: [
+  ],
+  exports: [NgRassemblerComponent]
+})
+export class NgRassemblerModule { }
